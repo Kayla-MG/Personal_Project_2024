ebee32b1feb5e4e4ca61ee2389aece3c1a66ece1
Kayla-MG <s00233714@atu.ie>
2024-04-29 10:36:44 +0100

    Add project files.

---
+++ Personal_Project_2024/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Personal_Project_2024
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //list of players
+        List<Player> allPlayers = new List<Player>();
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = playerSearch.Text.ToLower();
+
+           
+            //new list called filteredPlayers of object type player
+            //put it to lower to avoid issues with cp or lower
+            //search to see if search text matches anyone on list
+            //.Where is LINQ extension allows us to filter elements
+            // p=> is a lambda expression , p represents each Player object in allPlayers collection
+            List<Player> filteredPlayers = allPlayers.Where(p => p.Name.ToLower().Contains(searchText) || p.Position.ToLower().Contains(searchText)).ToList();
+
+            // Update players list box with the filtered player list
+            playerListView.ItemsSource = filteredPlayers;
+        }
+    }
+}
---
+++ Personal_Project_2024/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Project_2024
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public string Team { get; set; }
+        
+    }
+}
